115933d9448f680fdf42ba808d09d480d03467d2
Brandon Parks <musclemandude@gmail.com>
2019-05-07 11:22:35 -0400

    Removed unnecessary function

--- ar_coefficient_check.m
+++
@@ -1,7 +0,0 @@
-%% create a "ground-truth" signal, feed it back to the varm/estimate functions
-% This is to verify if the A(1,2) coefficient is 1?2 or 2?1
-
-truth_model=ar(1).estMdl;
-
-numperiods=length(x)-truth_model.P*2;
-Y=forecast(truth_model,numperiods,x(:,:,1));
