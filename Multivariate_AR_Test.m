daf1edf3095bd75a60ac71581307edd57ea94a57
Brandon Parks <musclemandude@gmail.com>
2019-05-13 11:36:17 -0400

    Automatically creates the r_n and R_n matrices for the multivariate Yule Walker. Tested up to 30 lags and 8 channels successfully.

--- Multivariate_AR_Test.m
+++ Multivariate_AR_Test.m
@@ -2,23 +2,23 @@ CCC;
 
 %% Create ground-truth models
 
-N = 100000;                 % Number of samples
-m = 2;                      % Model order
+N = 10000;                 % Number of samples
+m = 30;                      % Model order
 stdZ = 1;                   % Standard deviation of the error term
-d = 2;                      % Number of time series
+d = 8;                      % Number of time series
 % A = [0.5,0.001;0.003,0.4];  % MAR coefficients. Should be [d x d x m]
 A = zeros(d,d,m);
 I = eye(d);
 
 for i = 1:m
     randAR = rand(d);
-    A(:,:,i) = I.*(randAR - 0.5) + (1 - I).*((randAR - 0.5) * 0.05);
+    A(:,:,i) = I.*((randAR) - 0.5) / i + (1 - I).*((randAR - 0.5) * 0.05);
 end
 
 X = zeros(N + m,d);             % Data, should be [N x d]. 
 %   NOTE: Initiating with N + m so that the zeros at the beginning can be discarded
 
-for i = m+1:N
+for i = m+1:N+m
     for j = 1:m
         X(i,:) = X(i,:) + X(i-j,:) * A(:,:,j) + stdZ*randn(1,d);
     end
@@ -28,68 +28,43 @@ X = X(m+1:end,:);
 
 %% Estimate the AR coefficients
 
-R_all=zeros(m*2+1,m^2);
+R_all=zeros(m*2+1,d^2);
 
-% [R_xx,lags_xx] = xcov(X(:,1),m,'unbiased');
-% [R_yy,lags_yy] = xcov(X(:,2),m,'unbiased');
-% [R_xy,lags_xy] = xcov(X(:,1),X(:,2),m,'unbiased');
-% [R_yx,lags_yx] = xcov(X(:,2),X(:,1),m,'unbiased');
-
-for i=1:m^2
-    a=ceil(i/2);
-    b=mod(i-1,2)+1;
+for i=1:d^2
+    a=ceil(i/d);
+    b=mod(i-1,d)+1;
     [R_all(:,i),lags] = xcov(X(:,a),X(:,b),m,'unbiased');
 end
 
-r_n = zeros(m,m*d);
+r_n = zeros(d,m*d);
 
-for i=1:m
+for i=1:d
     for j=1:m*d
-        a=(i-1)*2 + mod(j-1,2)+1;
-        b=-ceil(j/2);
+        a=(i-1)*d + mod(j-1,d)+1;
+        b=-ceil(j/d);
+%         fprintf('(%d,%d) a = %d, b = %d\n',i,j,a,b);
         r_n(i,j)=R_all(lags==b,a);
     end
 end
-
-% r_n(1,1)=R_xx(lags_xx==-1);
-% r_n(1,2)=R_xy(lags_xy==-1);
-% r_n(1,3)=R_xx(lags_xx==-2);
-% r_n(1,4)=R_xy(lags_xy==-2);
-% r_n(2,1)=R_yx(lags_yx==-1);
-% r_n(2,2)=R_yy(lags_yy==-1);
-% r_n(2,3)=R_yx(lags_yx==-2);
-% r_n(2,4)=R_yy(lags_yy==-2);
 
 R_n = zeros(m*d);
 
 for i=1:m*d
     for j=1:m*d
-        a=mod((i-1)*2+mod(j-1,2),4)+1;
-        b=-floor((j-1)/2)*mod(ceil(i/2),2)+mod(ceil(j/2),2)*mod(ceil(i/2)-1,2);
+        a=mod((i-1)*d+mod(j-1,d),d^2)+1;
+        b=-floor((j-1)/d)+floor((i-1)/d);
+%         fprintf('(%d,%d) a = %d, b = %d\n',i,j,a,b);
         R_n(i,j)=R_all(lags==b,a);
     end
 end
 
-% R_n(1,1)=R_xx(lags_xx==0);
-% R_n(1,2)=R_xy(lags_xy==0);
-% R_n(1,3)=R_xx(lags_xx==-1);
-% R_n(1,4)=R_xy(lags_xy==-1);
-% R_n(2,1)=R_yx(lags_yx==0);
-% R_n(2,2)=R_yy(lags_yy==0);
-% R_n(2,3)=R_yx(lags_yx==-1);
-% R_n(2,4)=R_yy(lags_yy==-1);
-% R_n(3,1)=R_xx(lags_xx==1);
-% R_n(3,2)=R_xy(lags_xy==1);
-% R_n(3,3)=R_xx(lags_xx==0);
-% R_n(3,4)=R_xy(lags_xy==0);
-% R_n(4,1)=R_yx(lags_yx==1);
-% R_n(4,2)=R_yy(lags_yy==1);
-% R_n(4,3)=R_yx(lags_yx==0);
-% R_n(4,4)=R_yy(lags_yy==0);
-
 Theta_hat = reshape(r_n / R_n,d,d,m);
 
-A
+return
+
+%%
+
+A %#ok<*UNRCH>
 Theta_hat
-A - Theta_hat
+diff=A - Theta_hat;
 
