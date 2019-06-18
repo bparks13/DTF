c3cbcf06511bddafaaa5480fa5ec50bfcc734588
Brandon Parks <musclemandude@gmail.com>
2019-06-18 11:14:28 -0400

    Programmatically add paths so that all functions are visiblex

---
+++ dtf_startup.m
@@ -0,0 +1,11 @@
+CCC;
+
+currPath=mfilename('fullpath');
+rootPath=fileparts(currPath);
+
+addpath(fullfile(rootPath,'Calculate'),...
+    fullfile(rootPath,'Compare'),...
+    fullfile(rootPath,'Load'),...
+    fullfile(rootPath,'Output'));
+
+CCC;
