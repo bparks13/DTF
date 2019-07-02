40680acccf689debfeee9863bcfa407107320b72
Brandon Parks <musclemandude@gmail.com>
2019-07-02 10:26:21 -0400

    Instead of using the equation from the book, this now calculates the coefficients directly from the ACF. Not used though

--- Calculate/calculate_serial_coefficients.m
+++ Calculate/calculate_serial_coefficients.m
@@ -15,11 +15,27 @@ numChannels=size(E,2);
 
 phi_hat=zeros(numChannels,numChannels,order);
 
-for i=1:order
+% for i=1:order
 %     phi_hat(:,:,i) = (E((1+i):end,:)'*E(1:(end-i),:)) / (E(1:(end-i),:)'*E(1:(end-i),:));
-    phi_hat(:,:,i) = diag(diag((E((1+i):end,:)'*E(1:(end-i),:)) ./ (E(1:(end-i),:)'*E(1:(end-i),:))));
+%     phi_hat(:,:,i) = diag(diag((E((1+i):end,:)'*E(1:(end-i),:)) ./ (E(1:(end-i),:)'*E(1:(end-i),:))));
     % Errors should not be estimated across channels due to the supposed randomness of
     % channels
+% end
+
+for i=1:numChannels
+    phi_hat(i,i,:)=internal_calculate_acf(E(:,i),order);
 end
 
+%%
+    function acf=internal_calculate_acf(x,maxLag)
+        N=length(x);
+        nFFT = 2^(nextpow2(N)+1);
+        F = fft(x-mean(x),nFFT);
+        F = F.*conj(F);
+        acf = ifft(F);
+        acf = acf(1:(maxLag+1)); % Retain non-negative lags
+        acf = acf./acf(1); % Normalize
+        acf = real(acf);
+        acf=acf(2:end);
+    end
 end
