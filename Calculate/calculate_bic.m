302a0bcc3a5433f16466a2dda24adef25976df95
Brandon Parks <musclemandude@gmail.com>
2019-07-02 10:25:11 -0400

    Modified the algorithm to reflect the correct calculation

--- Calculate/calculate_bic.m
+++ Calculate/calculate_bic.m
@@ -1,11 +1,12 @@
-function bic=calculate_bic(logL,modelOrder,N)
-%% bic=calculate_bic(logL,modelOrder,N)
+function bic=calculate_bic(logL,modelOrder,numChannels,N)
+%% bic=calculate_bic(logL,modelOrder,numChannels,N)
 %
 %  Calculate the Bayesian Information Criterion for the AR model
 %
 %   Inputs:
 %    - logL: Log-likelihood of the model
 %    - modelOrder: Model order of the model
+%    - numChannels: Number of channels used in the model
 %    - N: Number of samples in the original dataset
 %
 %   Outputs:
@@ -14,7 +15,9 @@ function bic=calculate_bic(logL,modelOrder,N)
 %  See also: calculate_loglikelihood, estimate_residuals, estimate_ar_coefficients
 %
 
-bic = -2 * logL + modelOrder * log(N - modelOrder);
+numParams=numChannels^2 * modelOrder;
+
+bic = -2 * logL + numParams * log(N - modelOrder);
 % bic = 2 * logL + 2 * (modelOrder + 1) / N / log(N);
 
 end
