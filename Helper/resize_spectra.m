0435fdc05d2699c15bf593c24af8b7c451fc8938
Brandon Parks <musclemandude@gmail.com>
2019-08-05 11:47:49 -0400

    Helper function to resize the spectra into a Pxx format

---
+++ Helper/resize_spectra.m
@@ -0,0 +1,21 @@
+function P=resize_spectra(S)
+%% P=resize_spectra(S)
+%
+%  Given a matrix that contains both auto- and cross-spectra components, return only the
+%  auto spectral values. Additionally, P is defined as |S|
+%
+%   Inputs:
+%    - S: Matrix containing auto- and cross-spectral components. Size is [c x c x f],
+%       where c is the number of channels, and f is the number of frequencies
+%    
+%   Outputs:
+%    - P: Matrix containing only the auto-spectral components. Size is [f x c]
+%
+
+P=nan(size(S,3),size(S,1));
+
+for j=1:size(S,1)
+    P(:,j)=squeeze(abs(S(j,j,:)));
+end
+
+end
