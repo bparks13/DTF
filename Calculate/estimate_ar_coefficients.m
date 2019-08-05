daf8732af4dc3e171b35b933b9bbfe9590859c9d
Brandon Parks <musclemandude@gmail.com>
2019-08-05 10:37:19 -0400

    Added arfit as a method

--- Calculate/estimate_ar_coefficients.m
+++ Calculate/estimate_ar_coefficients.m
@@ -1,5 +1,5 @@
-function [AR,C]=estimate_ar_coefficients(X,m)
-%% [AR,C]=estimate_ar_coefficients(X,m)
+function [AR,C]=estimate_ar_coefficients(X,m,method)
+%% [AR,C]=estimate_ar_coefficients(X,m,method)
 %
 %  Given a signal and the model order desired, returns the AR coefficients (AR) and
 %  the variance of the error term from the estimated parameters (C)
@@ -8,6 +8,9 @@ function [AR,C]=estimate_ar_coefficients(X,m)
 %    - X: Vector of signals to estimate. Size is [N x d], where N is the number of samples
 %       and d is number of series. Can either be univariate or multivariate.
 %    - m: AR model order to estimate
+%    - method: String defining whether to use Yule-Walker equations ['yule', default] or
+%       to use the arfit methods from the Signal Processing for Neuroscientists book
+%       ['arfit'] 
 %
 %   Outputs:
 %    - AR: Estimated AR coefficients, size is [m x 1] for univariate signals, and 
@@ -18,6 +21,10 @@ function [AR,C]=estimate_ar_coefficients(X,m)
 %  See also: mvar, dtf, estimate_residuals
 %
 
+if nargin==2
+    method='yule';
+end
+
 if size(X,2) > 1
     isUnivariate=false;
     numSeries=size(X,2);   
@@ -25,67 +32,82 @@ else
     isUnivariate=true;
 end
 
-if isUnivariate
-    [rxx,lags] = xcov(X,m,'unbiased');
-    r_n = rxx(lags>0);
-    idx0 = find(lags==0);
-    R_n = zeros(m,m);
-
-    for i=1:m
-        tmpidx = idx0 -(i-1);
-        R_n(:,i) = rxx(tmpidx:tmpidx+m-1);
-    end
-
-    AR = R_n\r_n; % Predictor for AR coefficients
-    C = flip(rxx(lags<=0))'*[1; -AR];
-else
-    R_all=zeros(m*2+1,numSeries^2);
-
-    for i=1:numSeries^2
-        a=ceil(i/numSeries);
-        b=mod(i-1,numSeries)+1;
-%         fprintf('i = %d, a = %d, b = %d\n',i,a,b);
-        [R_all(:,i),lags] = xcov(X(:,a),X(:,b),m,'unbiased');
-    end
-
-    r_n = zeros(m*numSeries,numSeries);
+if strcmp(method,'yule')
     
-    for j=1:numSeries
+disp('WARNING: THIS METHOD IS BUGGY AND LIKELY INCORRECT')
+
+    if isUnivariate
+        [rxx,lags] = xcov(X,m,'unbiased');
+        r_n = rxx(lags>0);
+        idx0 = find(lags==0);
+        R_n = zeros(m,m);
+
+        for i=1:m
+            tmpidx = idx0 -(i-1);
+            R_n(:,i) = rxx(tmpidx:tmpidx+m-1);
+        end
+
+        AR = R_n\r_n; % Predictor for AR coefficients
+        C = flip(rxx(lags<=0))'*[1; -AR];
+    else
+        R_all=zeros(m*2+1,numSeries^2);
+
+        for i=1:numSeries^2
+            a=ceil(i/numSeries);
+            b=mod(i-1,numSeries)+1;
+    %         fprintf('i = %d, a = %d, b = %d\n',i,a,b);
+            [R_all(:,i),lags] = xcov(X(:,a),X(:,b),m,'unbiased');
+%             [R_all(:,i),lags] = xcov(X(:,a),X(:,b),m,'coeff');
+        end
+
+        r_n = zeros(m*numSeries,numSeries);
+
+        for j=1:numSeries
+            for i=1:m*numSeries
+    %             a=(j-1)*numSeries + mod(i-1,numSeries)+1;
+    %             b=-ceil(i/numSeries);
+                a=j+mod(i-1,numSeries)*numSeries;
+                b=ceil(i/numSeries);
+    %             fprintf('(%d,%d) a = %d, b = %d\n',i,j,a,b);
+                r_n(i,j)=R_all(lags==b,a);
+            end
+        end
+
+        R_n = zeros(m*numSeries);
+
         for i=1:m*numSeries
-%             a=(j-1)*numSeries + mod(i-1,numSeries)+1;
-%             b=-ceil(i/numSeries);
-            a=j+mod(i-1,numSeries)*numSeries;
-            b=ceil(i/numSeries);
-%             fprintf('(%d,%d) a = %d, b = %d\n',i,j,a,b);
-            r_n(i,j)=R_all(lags==b,a);
+            for j=1:m*numSeries
+                a=mod((i-1)*numSeries+mod(j-1,numSeries),numSeries^2)+1;
+                b=-floor((j-1)/numSeries)+floor((i-1)/numSeries);
+    %             fprintf('(%d,%d) a = %d, b = %d\n',i,j,a,b);
+                R_n(i,j)=R_all(lags==b,a);
+            end
+        end
+
+        theta = (R_n \ r_n);
+        AR=zeros(numSeries,numSeries,m);
+
+        for i=1:m
+            ind=(i-1)*numSeries+1:i*numSeries;
+            AR(:,:,i) = theta(ind,:);
+        end
+
+        if nargout > 1 
+            C=reshape(R_all(lags==0,:),numSeries,numSeries);
+
+            for i=1:m
+%                 C=C+reshape(R_all(lags==-i,:),numSeries,numSeries)' * -AR(:,:,i); 
+                C = C + AR(:,:,i) * reshape(R_all(x,:),numSeries,numSeries); 
+            end
         end
     end
-
-    R_n = zeros(m*numSeries);
-
-    for i=1:m*numSeries
-        for j=1:m*numSeries
-            a=mod((i-1)*numSeries+mod(j-1,numSeries),numSeries^2)+1;
-            b=-floor((j-1)/numSeries)+floor((i-1)/numSeries);
-%             fprintf('(%d,%d) a = %d, b = %d\n',i,j,a,b);
-            R_n(i,j)=R_all(lags==b,a);
-        end
-    end
+elseif strcmp(method,'arfit')
+    [~,AR_tmp,C,~,~,~]=arfit(X,m,m);
     
-    theta = (R_n \ r_n);
     AR=zeros(numSeries,numSeries,m);
     
     for i=1:m
-        ind=(i-1)*numSeries+1:i*numSeries;
-        AR(:,:,i) = theta(ind,:);
-    end
-     
-    if nargout > 1 
-        C=reshape(R_all(lags==0,:),numSeries,numSeries);
-
-        for i=1:m
-            C=C+reshape(R_all(lags==-i,:),numSeries,numSeries)' * -AR(:,:,i); 
-        end
+        AR(:,:,i)=AR_tmp(:,(i-1)*numSeries+1:i*numSeries);
     end
 end
 
