a16e1b7a8417ebe5522cb9726a34b5a5dadbff41
Brandon Parks <musclemandude@gmail.com>
2019-08-06 09:20:37 -0400

    Made it an even number of frequencies, easier to define the spectral range

--- ExternalFunctions/pr14_2.m
+++ ExternalFunctions/pr14_2.m
@@ -8,6 +8,7 @@ dt=1/sr;                    % sample interval
 Nyq=sr/2;                   % Nyquist frequency
 T=10;                       % Epoch of 10s
 F=0:1/T:Nyq;                % Frequency Scale
+F=F(1:end-1);
 % the A matrices for the autoregressive process
 A1=[.5 0 0;0 .8 0;0 0 .7];  % x1, x2, and x3 depend on their own k-1 values
 A2=[-.8 0 0;0 0 0;0 0 0];   % only x1 depends on its own k-2 value
@@ -29,7 +30,7 @@ end
 
 %%
 k=0;
-for f=0:1/T:Nyq                                     % LOOP to compute H
+for f=F                                     % LOOP to compute H
     k=k+1;
     % compute the z values for each delay and use the imaginary part
     % j*2*pi*f*dt to obtain the values for the frequency response
@@ -66,13 +67,13 @@ for n=1:3
         if n==m
             ttl=['Spectrum ' num2str(m)];
             % put the plot variable [the Spectrum] in a temporary array
-            for k=1:5001;tmp(k)=H2(n,m,k);end
+            for k=1:5000;tmp(k)=H2(n,m,k);end
             ymax=max(1.*tmp);
         else
             ttl=[' ' num2str(m) '->' num2str(n)];
             DTF(n,m,:)=H2(n,m,:)./sum(H2(n,:,:));       % Eq (14.7)
             % put the plot variable [the DTF] in a temporary array
-            for k=1:5001;tmp(k)=DTF(n,m,k);end
+            for k=1:5000;tmp(k)=DTF(n,m,k);end
             ymax=1.;
         end
         subplot(4,3,n*3+m);
