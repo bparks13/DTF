99b0349e2e7239556673a6f82032bb1dddfd8c97
bparks <b.parks@ufl.edu>
2019-08-09 13:56:06 -0400

    Add the Files folder if it doesn't exist locally

--- dtf_startup.m
+++ dtf_startup.m
@@ -7,4 +7,8 @@ addpath(fullfile(rootPath,'Calculate'),...
     fullfile(rootPath,'Load'),...
     fullfile(rootPath,'Output'));
 
+if exist('Files','dir') == 0
+    mkdir(fullfile(rootPath,'Files'));
+end
+
 CCC;
