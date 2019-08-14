7cdfce095ca277a65198e99ef92b389567b4f471
Brandon Parks <musclemandude@gmail.com>
2019-08-14 11:39:28 -0400

    If realizations are used, drop all realizations from the trial in question

--- Load/drop_trials.m
+++ Load/drop_trials.m
@@ -1,5 +1,5 @@
-function trialsToDrop=drop_trials(file,condition)
-%% trialsToDrop=drop_trials(file,condition)
+function trialsToDrop=drop_trials(file,condition,numRealizations)
+%% trialsToDrop=drop_trials(file,condition,numRealizations)
 %
 %  Return the indices of trials to drop due to artifact or other problems. If there are no
 %  trials to drop, trialsToDrop will be empty
@@ -8,6 +8,8 @@ function trialsToDrop=drop_trials(file,condition)
 %    - file: String containing the absolute file path. Not matching any particular
 %       portions, but rather the whole name
 %    - condition: Single value pertaining to the condition from which to drop the trials
+%    - numRealizations: Int defining the number of realizations that each trial was split
+%       into. If no number is given, the default value is 1
 %
 %   Outputs:
 %    - trialsToDrop: Vector containing the indices of trials to drop. If there are none to
@@ -16,11 +18,18 @@ function trialsToDrop=drop_trials(file,condition)
 %  See also: processing_pipeline, load_data
 %
 
+% warning('load_data?drop_trials. ADD A CHECK HERE IF THE TRIALS ARE SEPARATED INTO REALIZATIONS OR NOT');
+
+if nargin==2
+    numRealizations=1;
+end
+
 trialsToDrop=[];
 
 if strcmp(file,'\\gunduz-lab.bme.ufl.edu\Study_Tourette\TS04 Double DBS Implantation\2017_03_01\preproc\run16')
     if condition==1
-        trialsToDrop=9;
+        trials=9;
+        trialsToDrop=(trials-1)*numRealizations+1:trials*numRealizations;
     end
 end
 
