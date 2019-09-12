ba65c95b84a96ee6605db9d2f7403a5bac26de90
Brandon Parks <musclemandude@gmail.com>
2019-09-12 10:36:54 -0400

    Misc

--- Compare/analysisOfSurrogateAnalysis.m
+++ Compare/analysisOfSurrogateAnalysis.m
@@ -4,56 +4,56 @@ CCC;
 
 %% Load rat hippocampal data
 
-% data=load('\\gunduz-lab.bme.ufl.edu\Lab\Sarah\Classes\Multivariate\Project\Ding_Data\laminar_rat_3.7mm_0.7mA_bipolar_CA1_DG.mat');
-% X=permute(data.XX,[3 1 2]);
-% numSamples=size(X,1);
-% numChannels=size(X,2);
-% numRealizations=size(X,3);
-% fs=200;
-% 
-% [num,den]=CreateBSF_butter(fs,3,[58 62]);
-% 
-% for i=1:numRealizations
-%     X(:,:,i)=filtfilt(num,den,X(:,:,i));
-%     
-%     X(:,1,i)=X(:,1,i)./std(X(:,1,i));
-%     X(:,2,i)=X(:,2,i)./std(X(:,2,i));
-% end
-% 
+data=load('\\gunduz-lab.bme.ufl.edu\Lab\Sarah\Classes\Multivariate\Project\Ding_Data\laminar_rat_3.7mm_0.7mA_bipolar_CA1_DG.mat');
+X=permute(data.XX,[3 1 2]);
+numSamples=size(X,1);
+numChannels=size(X,2);
+numRealizations=size(X,3);
+fs=200;
+
+[num,den]=CreateBSF_butter(fs,3,[58 62]);
+
+for i=1:numRealizations
+    X(:,:,i)=filtfilt(num,den,X(:,:,i));
+    
+    X(:,1,i)=X(:,1,i)./std(X(:,1,i));
+    X(:,2,i)=X(:,2,i)./std(X(:,2,i));
+end
+
 % Calculate values based on pre-determined model orders
-% 
+
 % order=18; % Model order of 18 for BIC
-% freqRange=0:0.1:15;
-% 
-% config=struct(...
-%     'orderRange',18,...
-%     'crit','bic',...
-%     'method','arfit',...
-%     'fs',fs,...
-%     'orderSelection','min',...
-%     'freqRange',freqRange,...
-%     'logLikelihoodMethod',2 ...
-%     );
-% 
-% mdl=struct;
-% E=struct;
-% gamma=struct('bic',nan(numChannels,numChannels,length(freqRange),numRealizations));
-% H=struct('bic',nan(numChannels,numChannels,length(freqRange),numRealizations));
-% S=struct('bic',nan(numChannels,numChannels,length(freqRange),numRealizations));
-% 
-% for i=1:numRealizations
-%     [mdl(i).bic,E(i).bic,~]=mvar(X(:,:,i),config);
-%     [gamma.bic(:,:,:,i),H.bic(:,:,:,i)]=dtf(mdl(i).bic,freqRange,fs);
-%     S.bic(:,:,:,i)=calculate_spectra(H.bic(:,:,:,i),mdl(i).bic.C);
-% end
+freqRange=0:0.1:15;
+
+config=struct(...
+    'orderRange',1:20,...
+    'crit','bic',...
+    'method','arfit',...
+    'fs',fs,...
+    'orderSelection','min',...
+    'freqRange',freqRange,...
+    'logLikelihoodMethod',2 ...
+    );
+
+mdl=struct;
+E=struct;
+gamma=struct('bic',nan(numChannels,numChannels,length(freqRange),numRealizations));
+H=struct('bic',nan(numChannels,numChannels,length(freqRange),numRealizations));
+S=struct('bic',nan(numChannels,numChannels,length(freqRange),numRealizations));
+
+for i=1:numRealizations
+    [mdl(i).bic,E(i).bic,~]=mvar(X(:,:,i),config);
+    [gamma.bic(:,:,:,i),H.bic(:,:,:,i)]=dtf(mdl(i).bic,freqRange,fs);
+    S.bic(:,:,:,i)=calculate_spectra(H.bic(:,:,:,i),mdl(i).bic.C);
+end
 
 %% Save this file setup
 
 fileName=fullfile(get_root_path,'Files','rat_data_for_surrogate_analysis.mat');
 
-% save(fileName,'-v7.3');
+save(fileName,'-v7.3');
 
-load(fileName);
+% load(fileName);
 
 %% Set values for surrogate analysis with randomized sample replacement
 
@@ -80,14 +80,14 @@ config_surr=struct('iterations',1000,'numSamples',1,'method','sample');
 
 %% Run surrogate analysis with randomized sample replacement
 
-% for i=1:numSamples
-%     fprintf('Beginning %d samples...\n',i);
-%     config_surr.numSamples=i;
-%     [tmp_surr,tmp_dist,tmp_pxx]=surrogate_analysis(file,config_surr);
-%     surrogate(i).bic=tmp_surr.rat;
-%     distribution(i).bic=tmp_dist.rat;
-%     pxx(i).bic=tmp_pxx.rat;
-% end
+for i=1:numSamples
+    fprintf('Beginning %d samples...\n',i);
+    config_surr.numSamples=i;
+    [tmp_surr,tmp_dist,tmp_pxx]=surrogate_analysis(file,config_surr);
+    surrogate(i).sample=tmp_surr.rat;
+    distribution(i).sample=tmp_dist.rat;
+    pxx(i).sample=tmp_pxx.rat;
+end
 
 [sample_surr,sample_dist,sample_pxx]=surrogate_analysis(file,config_surr);
 
