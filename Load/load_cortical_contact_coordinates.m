1faed76b543b0e70c606d4ef006da22fc9bbb75c
Brandon Parks <musclemandude@gmail.com>
2019-10-09 17:39:07 -0400

    Added function to keep track of cortical contact locations with respect to the *.pial files

---
+++ Load/load_cortical_contact_coordinates.m
@@ -0,0 +1,30 @@
+function c=load_cortical_contact_coordinates(subjID)
+%% c=load_cortical_contact_coordinates(subjID)
+%
+%  Load the coordinates of the cortical contacts as they are found in freesurfer
+%
+%   Inputs:
+%    - subjID: String containing the ID of the subject (either their absolute ID or the
+%       relative ID, 'ET_CL_002' or 'S1', respectively)
+%
+%   Outputs:
+%    - c: Matrix of coordinates corresponding to the XYZ coordinates of the cortical
+%       contacts found on the CT scan. Size is [n x 3], where n is the number of contacts
+%       and the three dimensions are in order, XYZ
+%
+
+if strcmp(subjID,'ET_CL_002') || strcmp(subjID,'S1')
+    c = [-37.16, -35.27, 47.67;
+        -37.33, -45.39, 45.47;
+        -36.50, -55.23, 42.75;
+        -35.16, -64.96, 39.48];
+elseif strcmp(subjID,'ET_CL_004') || strcmp(subjID,'S2')
+    c = [-42.94, -33.01, 56.25;
+        -44.82, -43.21, 55.20;
+        -46.38, -53.34, 52.94;
+        -48.31, -62.87, 51.57];
+elseif strcmp(subjID,'ET_OR_018') || strcmp(subjID,'S3')
+    c = [];
+end
+
+end
