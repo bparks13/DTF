e1061364aae9b4d85d592bcca38762c57b5d604f
Brandon Parks <musclemandude@gmail.com>
2020-03-03 16:41:37 -0500

    Added S05

--- Load/load_cortical_contact_coordinates.m
+++ Load/load_cortical_contact_coordinates.m
@@ -5,7 +5,7 @@ function c=load_cortical_contact_coordinates(subjID)
 %
 %   Inputs:
 %    - subjID: String containing the ID of the subject (either their absolute ID or the
-%       relative ID, 'ET_CL_002' or 'S1', respectively)
+%       relative ID, 'ET_CL_002' or 'S01', respectively)
 %
 %   Outputs:
 %    - c: Matrix of coordinates corresponding to the XYZ coordinates of the cortical
@@ -13,23 +13,30 @@ function c=load_cortical_contact_coordinates(subjID)
 %       and the three dimensions are in order, XYZ
 %
 
-if strcmp(subjID,'ET_CL_002') || strcmp(subjID,'S1')
+if strcmp(subjID,'ET_CL_002') || strcmp(subjID,'S01')
     c = [-37.16, -35.27, 47.67;
         -37.33, -45.39, 45.47;
         -36.50, -55.23, 42.75;
         -35.16, -64.96, 39.48];
-elseif strcmp(subjID,'ET_CL_004') || strcmp(subjID,'S2')
+elseif strcmp(subjID,'ET_CL_004') || strcmp(subjID,'S02')
     c = [-42.94, -33.01, 56.25;
         -44.82, -43.21, 55.20;
         -46.38, -53.34, 52.94;
         -48.31, -62.87, 51.57];
-elseif strcmp(subjID,'ET_OR_018') || strcmp(subjID,'S3')
+elseif strcmp(subjID,'ET_OR_018') || strcmp(subjID,'S03')
     c = [-35.84, 3.21, 59.86;
         -34.76, -4.59, 66.71;
         -32.69, -12.86, 71.00;
         -29.04, -21.05, 74.39;
         -25.23, -30.02, 77.24;
         -21.06, -39.46, 78.29];
+elseif strcmp(subjID,'ET_CL_001') || strcmp(subjID,'S05')
+    c = [20.12, -81.84, 38.73;
+        22.94, -73.31, 46.13;
+        26.95, -64.62, 50.25;
+        29.52, -53.84, 52.33];
+else
+    c=nan;
 end
 
 end
