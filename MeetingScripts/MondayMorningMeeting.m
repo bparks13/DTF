2cfee6c31c8dbe1fcdfa7e41bb3af9832047eeae
Brandon Parks <musclemandude@gmail.com>
2020-04-07 14:08:34 -0400

    Script to run and generate the figures needed for presenting at the monday meeting

---
+++ MeetingScripts/MondayMorningMeeting.m
@@ -0,0 +1,41 @@
+% Monday Morning Meeting - 04/13/2020?
+
+%% Load file
+
+load('Files\S02_D01_R01');
+
+%% Plot all connectivity, resize, and print the figure
+
+currCond=cond_labels{1};
+config_plot.figTitle=sprintf('%s, %s, %s - %s: Connectivity (Decorrelated)',PATIENT_ID,RECORDING_DATE,RUN_ID,currCond);
+series=struct('original',x_filt.(currCond),'surrogate',surrogate_filt.(currCond));
+config_plot.surr_params.highlightSignificance=true;
+config_plot.surr_params.threshold=alpha;
+plot_connectivity(gamma_filt.(currCond),series,freqForAnalysis,contactNames,config_plot);
+
+hFig=gcf;
+hFig.Position=[hFig.Position(1:2),900,650];
+
+PrintFigure('Pictures\MondayMeeting\S02_D01_R01_Rest_Connectivity');
+
+%% Setup the barplots, plot them, resize, and print
+
+numConditions=length(cond_labels);
+freqBands=[4,8;8,12;12,20;20,30;30,45;45,70;70,100];
+freqBandIndices=convert_from_hertz_to_indices(freqBands,freqForAnalysis);
+freqBandLabel={'Theta','Alpha','Low Beta','High Beta','Low Gamma','Mid Gamma','High Gamma'};
+significance=calculate_significance_from_surrogate(surrogate_filt,alpha,'invariant');
+
+config=struct;
+config.hideUselessConnections=true;
+config.usefulConnections=returnUsefulConnections(freqBandIndices,gamma_filt,significance);
+
+for i=1:size(freqBands,1)
+    config.title=sprintf('%s Band Connectivity',freqBandLabel{i});
+    plot_bar_with_error(freqBandIndices{i},gamma_filt,contactNames,significance,config);
+    
+    hFig=gcf; hFig.Position=[hFig.Position(1:2),900,400];
+    PrintFigure(sprintf('Pictures\\MondayMeeting\\S02_D01_R01_BarPlot_%s',freqBandLabel{i}))
+end
+
+
